fdaaec141028c7183007dba225c88c2fc0661f47
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-19 20:17:42 -0500

    POO INICIALIZANDO SOLUCIÓN

---
+++ Clases_Basico/Humano.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Clases_Basico
+{
+    public class Humano
+    {
+
+            //Miembro variable
+         public string primerNombre;
+        public string apellido;
+        
+        //Miembro método
+        public void presentarme()
+        {
+            Console.WriteLine("Hola, soy {0} {1} " , primerNombre, apellido);
+        }
+    }
+}
---
+++ Clases_Basico/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Clases_Basico
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //crea un objeto desde mi clase
+            //una instancia desde la clase Humano    
+            Humano Marco = new Humano();
+            Humano Antonio = new Humano();
+
+            //Acceder desde una variable pública
+            Marco.primerNombre = "Marco";
+            Antonio.primerNombre = "Antonio";
+
+            Marco.apellido = "Huerta";
+            Antonio.apellido = "Sánchez";
+            
+            //llama un método desde una clase
+            Marco.presentarme();
+            Antonio.presentarme();
+
+            Console.Read();
+        }
+
+        
+    }
+}
